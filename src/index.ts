1fa262477c5af189f3fd8a57a732062ac4e66297
sachetwasti1997 <sachetwasti61@gmail.com>
2023-05-25 14:27:40 +0530

    Added mongo URL

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app = express();
 
 const start = async () => {
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    await mongoose.connect('mongodb://tickets-mongo-srv:27017/tickets');
     console.log('Connected to mongodb');
   } catch (err) {
     console.log(err);
